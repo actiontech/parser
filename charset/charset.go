9b2a278d3432664644d9725fac346336d0f9e4e3
Fu Wenhui <_@_._>
2020-12-07 20:13:14 +0800

    add some charset

--- charset/charset.go
+++ charset/charset.go
@@ -63,6 +63,8 @@ var charsetInfos = []*Charset{
 	{CharsetASCII, CollationASCII, make(map[string]*Collation), "US ASCII", 1},
 	{CharsetLatin1, CollationLatin1, make(map[string]*Collation), "Latin1", 1},
 	{CharsetBin, CollationBin, make(map[string]*Collation), "binary", 1},
+	{CharsetGbk, CollationGbk, make(map[string]*Collation), "gbk", 1},
+	{CharsetLatin2, CollationLatin2, make(map[string]*Collation), "Latin2", 1},
 }
 
 // All the names supported collations should be in the following table.
@@ -72,6 +74,8 @@ var supportedCollationNames = map[string]struct{}{
 	CollationASCII:   {},
 	CollationLatin1:  {},
 	CollationBin:     {},
+	CollationGbk:     {},
+	CollationLatin2:  {},
 }
 
 // Desc is a charset description.
@@ -157,6 +161,10 @@ func GetCharsetDesc(cs string) (*Desc, error) {
 		return descs[3], nil
 	case CharsetBin:
 		return descs[4], nil
+	case CharsetGbk:
+		return descs[5], nil
+	case CharsetLatin2:
+		return descs[6], nil
 	default:
 		return nil, errors.Errorf("Unknown charset %s", cs)
 	}
@@ -207,6 +215,11 @@ const (
 	CharsetLatin1 = "latin1"
 	// CollationLatin1 is the default collation for CharsetLatin1.
 	CollationLatin1 = "latin1_bin"
+
+	CharsetGbk   = "gbk"
+	CollationGbk = "gbk_bin"
+	CharsetLatin2 = "latin2"
+	CollationLatin2 = "latin2_bin"
 )
 
 var collations = []*Collation{
